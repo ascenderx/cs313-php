b9027021e4eba3d79345a69b8307136cb05f7b48
James D. Downer <dow16002@byui.edu>
2018-09-25 10:56:03 -0600

    Add more robust checks for Team02 buttons

--- web/week02/team02-a.js
+++ web/week02/team02-a.js
@@ -11,16 +11,34 @@ function gtag(tagName) {
 }
 
 const divsBlock = gcls('block');
+const blk1 = divsBlock[0];
 const btClickMe = gel('btClickMe');
 const txtColor = gel('txtColor');
 const btChangeColor = gel('btChangeColor');
+
+let colorDefault;
+let textDefault;
+
+window.addEventListener('load', function() {
+   colorDefault = blk1.style.backgroundColor;
+   textDefault = blk1.innerText;
+});
 
 btClickMe.addEventListener('click', function() {
    alert('Clicked!');
 });
 
 btChangeColor.addEventListener('click', function() {
-   let color = txtColor.value;
-   divsBlock[0].style.backgroundColor = color;
-   divsBlock[0].innerText = color;
+   let color = txtColor.value.trim();
+   if (color.length == 0) {
+      blk1.style.backgroundColor = colorDefault;
+      blk1.innerText = textDefault;
+   } else {
+      blk1.style.backgroundColor = color;
+      blk1.innerText = color;
+   }
+});
+
+txtColor.addEventListener('change', function() {
+   this.value = this.value.trim();
 });
