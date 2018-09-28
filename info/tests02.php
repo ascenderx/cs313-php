704cf63a353e05d4a5ee0b096d8d557777eb93db
downerj <downer.james.256k@gmail.com>
2018-09-28 12:38:17 -0600

    Begin homepage week02

---
+++ info/tests02.php
@@ -0,0 +1,5 @@
+<?php
+    $value = 4 + "cat";
+    print('the value is $value');
+    print("\nthe value is $value\n");
+?>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Jimmy Downer</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="./top.css" />
+        <link rel="shortcut icon" type="image/png" href="./assets/images/php-512.png" />
+        
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    </head>
+    <body>
+        <div class="u-header">
+            <div class="u-heading-1 code">Jimmy Downer @ Heroku</div>
+        </div>
 
-  phpinfo();
+        <!-- <div class="u-content">
+            <a href="#">
+                <div class="u-button">Home</div>  
+            </a>
+        </div> -->
 
-?>
+        <div class="u-content">
+            <div class="u-heading-2">Your OS Info</div>
+            <hr />
+            <div class="code"><?php include "osinfo.php" ?></div>
+        </div>
+    </body>
+</html>
